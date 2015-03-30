bb6e8b3f7a15cf5dd156c1a3a3e38634c6437dc3
giuliocam <giucamilleri@gmail.com>
2015-03-30 15:24:10 +0200

    Initial commit

---
+++ src/main/java/um/edu/mt/App.java
@@ -0,0 +1,14 @@
+package um.edu.mt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    //hello
+}
---
+++ src/test/java/um/edu/mt/AppTest.java
@@ -0,0 +1,19 @@
+package um.edu.mt;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase{
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
