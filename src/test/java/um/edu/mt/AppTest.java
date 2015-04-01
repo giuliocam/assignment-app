cf76a2b5af7f7d3c7eb9efe3875fd2a9efb3d48f
giuliocam <giucamilleri@gmail.com>
2015-04-01 18:33:16 +0200

    id and balance tests

--- src/test/java/um/edu/mt/AppTest.java
+++ src/test/java/um/edu/mt/AppTest.java
@@ -1,5 +1,6 @@
 package um.edu.mt;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -8,9 +9,35 @@ import org.junit.Test;
 public class AppTest {
 
     @Test
-    public void initialTest() {
-        App.main();
-        System.out.println("...");
+    public void uniqueIDTest() {
+
+        AccountDatabase aDB = new AccountDatabase();
+        Account a = new Account(1, "xyz", 1000);
+        Account b = new Account(2, "xyz", 2000);
+        Account c = new Account(1, "xyz", 3000);
+
+        Assert.assertTrue(aDB.addAccount(a));
+        Assert.assertTrue(aDB.addAccount(b));
+        Assert.assertFalse(aDB.addAccount(c));
     }
+
+    @Test
+    public void balanceTest() {
+
+        AccountDatabase aDB = new AccountDatabase();
+        TransactionManager tm = new TransactionManager(aDB);
+
+        Account a = new Account(1, "xyz", 1000);
+        //Account b = new Account(2, "xyzz", 2000);
+        Account c = new Account(3, "xyzzz", 3000);
+
+        aDB.addAccount(a);
+        aDB.addAccount(c);
+
+        Assert.assertFalse(tm.processTransaction(1, 3, 2000));
+
+
+    }
+
 
 }
